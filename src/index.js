433928f48fcd57f3ee8deffb7b07fc45f68c74c6
DESKTOP-IVVQ3M0\Ilya <ilya.matveev007@gmail.com>
2020-10-18 20:40:04 +0400

    feat:reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = n ** 2;
+    n = n**0.5;
+    n += '';
+    var ans = '';
+    for (var i = n.length - 1; i > -1; i--){
+        ans += n[i];
+    }
+    return Number(ans);
 }
